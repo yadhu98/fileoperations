2b60b9987f6496f1c1b04694f001e7625c2a6f34
YADHU KRISHNAN <yadhu98@gmail.com>
2026-02-28 06:52:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,63 @@
+def readFile(file):
+    try:
+        file.seek(0)
+        r =file.read()
+        print(r)
+    finally:
+        file.close()
+
+def writeFile(file,string):
+     try:
+        file.write(' {}'.format(string))
+        file.seek(0)
+        rd = file.read()
+        print(rd)
+     finally:
+        file.close()
+
+def countWords(file):
+     try:
+        file.seek(0)
+        r = file.read()
+        cumulativewordcount=0
+        for item in r:
+            if(item == ' '):
+                cumulativewordcount+=1
+        cumulativelettercount = 0
+        for item in r:
+            if(item != ' '):
+                cumulativelettercount+=1
+        print('letter count: {}....word count {}'.format(cumulativelettercount,cumulativewordcount+1))
+    
+     finally:
+        file.close()
+
+
+def clearFile(file):
+    try:
+        file.seek(0)
+        file.truncate()
+    finally:
+        file.close()
+
+
+while True:
+    try:
+        option = int(input("Choose an option\n 1) Write 2) Read 3) Count 4) Clear file 5)Exit   "))
+        if(option == 5):
+            break
+        with open("./test.txt","a+") as f:
+            if(option == 1):
+                sentences= input('Enter some script')
+                writeFile(f,sentences)
+            elif(option == 2):
+                readFile(f)
+            elif(option == 3):
+                countWords(f)
+            elif(option == 4):
+                clearFile(f)
+            else:
+                print("Invalid Input")
+    except ValueError:
+        print("!! Invalid Input. Please enter a number (1, 2, 3, 4 or 5).")
+        print("-" * 30)
